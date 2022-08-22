93ce513cbcf84a925a9ba9c803b2dde13975a91e
successdevop <successraphael28@gmail.com>
2022-08-22 19:39:56 +0100

    created my entities and refactored using Lombok

--- src/main/java/com/success/reddit/RedditApplication.java
+++ src/main/java/com/success/reddit/RedditApplication.java
@@ -1,7 +1,10 @@
 package com.success.reddit;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class RedditApplication {
@@ -10,4 +13,11 @@ public class RedditApplication {
 		SpringApplication.run(RedditApplication.class, args);
 	}
 
+	@Bean
+	@Profile("dev")
+	CommandLineRunner runner(){
+		return args -> {
+			System.out.println("We only run this in dev");
+		};
+	}
 }
---
+++ src/main/java/com/success/reddit/models/Comment.java
@@ -0,0 +1,22 @@
+package com.success.reddit.models;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Comment {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NonNull
+    private String commentBody;
+}
